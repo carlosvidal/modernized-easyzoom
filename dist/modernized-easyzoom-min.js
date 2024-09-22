2ef112448ca83ea45bfcfc33f78abb1954f0f867
Carlos Vidal <carlos@mitienda.pe>
2024-09-21 22:13:38 -0500

    demo

--- dist/modernized-easyzoom-min.js
+++ dist/modernized-easyzoom-min.js
@@ -1,1 +1,151 @@
-export default class EasyZoom{constructor(t,e={}){this.target=t,this.opts={...EasyZoom.defaults,...e,...this.getDataOptions()},this._init()}static defaults={loadingNotice:"Loading image",errorNotice:"The image could not be loaded",errorDuration:2500,linkAttribute:"href",preventClicks:!0,beforeShow:()=>{},beforeHide:()=>{},onShow:()=>{},onHide:()=>{},onMove:()=>{}};getDataOptions(){const t=this.target.dataset,e={};for(const[i,s]of Object.entries(t))e[i]="true"===s||"false"!==s&&s;return e}_init(){this.isOpen=!1,this.isReady=!1,this.link=this.target.querySelector("a"),this.image=this.target.querySelector("img"),this.flyout=document.createElement("div"),this.flyout.className="easyzoom-flyout",this.target.style.position="relative",this.target.appendChild(this.flyout),this.target.addEventListener("mouseenter",this._onEnter.bind(this)),this.target.addEventListener("mousemove",this._onMove.bind(this)),this.target.addEventListener("mouseleave",this._onLeave.bind(this))}_onEnter(t){this.isReady?(this.isOpen=!0,this.target.classList.add("is-active"),this._move(t)):this._loadZoomImage((()=>{this.isOpen=!0,this.target.classList.add("is-active"),this._move(t)}))}_loadZoomImage(t){this.isReady?t():(this.zoomImg=new Image,this.zoomImg.src=this.link.getAttribute(this.opts.linkAttribute),this.zoomImg.onload=()=>{this.isReady=!0,this.flyout.appendChild(this.zoomImg),this.zoomImg.style.position="absolute",t()})}_onMove(t){this.isOpen&&this.isReady&&this._move(t)}_onLeave(){this.isOpen=!1,this.target.classList.remove("is-active")}_move(t){const e=this.target.getBoundingClientRect(),i=(t.clientX-e.left)/e.width,s=(t.clientY-e.top)/e.height,o=-(this.zoomImg.width-this.flyout.offsetWidth)*i,a=-(this.zoomImg.height-this.flyout.offsetHeight)*s;this.zoomImg.style.transform=`translate(${o}px, ${a}px)`,this.opts.onMove.call(this,o,a)}teardown(){this.target.removeEventListener("mouseenter",this._onEnter),this.target.removeEventListener("mousemove",this._onMove),this.target.removeEventListener("mouseleave",this._onLeave),this.flyout.remove()}swap(t,e){this.isReady=!1,this.image.src=t,this.link.setAttribute(this.opts.linkAttribute,e)}}
+// EasyZoom.js
+export default class EasyZoom {
+  constructor(target, options = {}) {
+    this.target = target;
+    this.opts = { ...EasyZoom.defaults, ...options, ...this.getDataOptions() };
+    this._init();
+  }
+
+  static defaults = {
+    loadingNotice: "Loading image",
+    errorNotice: "The image could not be loaded",
+    errorDuration: 2500,
+    linkAttribute: "href",
+    preventClicks: true,
+    onShow: () => {},
+    onHide: () => {},
+    onMove: () => {},
+  };
+
+  getDataOptions() {
+    const dataset = this.target.dataset;
+    const options = {};
+    for (const [key, value] of Object.entries(dataset)) {
+      options[key] =
+        value === "true" ? true : value === "false" ? false : value;
+    }
+    return options;
+  }
+
+  _init() {
+    this.isOpen = false;
+    this.isReady = false;
+
+    this.link = this.target.querySelector("a");
+    this.image = this.target.querySelector("img");
+
+    this.flyout = document.createElement("div");
+    this.flyout.className = "easyzoom-flyout";
+
+    this.target.style.position = "relative";
+    this.target.appendChild(this.flyout);
+
+    this.target.addEventListener("mouseenter", this._onEnter.bind(this));
+    this.target.addEventListener("mousemove", this._onMove.bind(this));
+    this.target.addEventListener("mouseleave", this._onLeave.bind(this));
+
+    // Add touch event listeners
+    this.target.addEventListener("touchstart", this._onEnter.bind(this));
+    this.target.addEventListener("touchmove", this._onMove.bind(this));
+    this.target.addEventListener("touchend", this._onLeave.bind(this));
+
+    // Prevent default link behavior
+    this.link.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+
+    this._loadZoomImage();
+  }
+
+  _loadZoomImage() {
+    this.zoomImg = new Image();
+    this.zoomImg.src = this.link.getAttribute(this.opts.linkAttribute);
+    this.zoomImg.onload = () => {
+      this.isReady = true;
+      this.flyout.appendChild(this.zoomImg);
+      this.zoomImg.style.position = "absolute";
+    };
+  }
+
+  _onEnter(e) {
+    e.preventDefault();
+    if (!this.isReady) return;
+    this.isOpen = true;
+    this.target.classList.add("is-active");
+    this._move(e);
+  }
+
+  _onMove(e) {
+    e.preventDefault();
+    if (!this.isOpen || !this.isReady) return;
+    this._move(e);
+  }
+
+  _onLeave(e) {
+    this.isOpen = false;
+    this.target.classList.remove("is-active");
+  }
+
+  _move(e) {
+    let pointerX, pointerY;
+
+    if (e.type.startsWith("touch")) {
+      const touch = e.touches[0] || e.changedTouches[0];
+      pointerX = touch.clientX;
+      pointerY = touch.clientY;
+    } else {
+      pointerX = e.clientX;
+      pointerY = e.clientY;
+    }
+
+    const targetRect = this.target.getBoundingClientRect();
+    const relativePositionX = (pointerX - targetRect.left) / targetRect.width;
+    const relativePositionY = (pointerY - targetRect.top) / targetRect.height;
+
+    const moveX = -(
+      (this.zoomImg.width - this.flyout.offsetWidth) *
+      relativePositionX
+    );
+    const moveY = -(
+      (this.zoomImg.height - this.flyout.offsetHeight) *
+      relativePositionY
+    );
+
+    this.zoomImg.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    this.opts.onMove.call(this, moveX, moveY);
+  }
+
+  teardown() {
+    this.target.removeEventListener("mouseenter", this._onEnter);
+    this.target.removeEventListener("mousemove", this._onMove);
+    this.target.removeEventListener("mouseleave", this._onLeave);
+    this.target.removeEventListener("touchstart", this._onEnter);
+    this.target.removeEventListener("touchmove", this._onMove);
+    this.target.removeEventListener("touchend", this._onLeave);
+    this.flyout.remove();
+  }
+
+  swap(standardSrc, zoomHref) {
+    this.isReady = false;
+
+    // Update the standard image
+    this.image.src = standardSrc;
+
+    // Update the zoom link
+    this.link.setAttribute(this.opts.linkAttribute, zoomHref);
+
+    // Load the new zoom image
+    const newZoomImg = new Image();
+    newZoomImg.src = zoomHref;
+    newZoomImg.onload = () => {
+      this.zoomImg = newZoomImg;
+      this.zoomImg.style.position = "absolute";
+
+      // Clear the flyout and add the new image
+      this.flyout.innerHTML = "";
+      this.flyout.appendChild(this.zoomImg);
+
+      this.isReady = true;
+    };
+  }
+}
--- src/modernized-easyzoom.js
+++ src/modernized-easyzoom.js
@@ -1,27 +1,22 @@
-// Modernized EasyZoom.js
+// EasyZoom.js
 export default class EasyZoom {
   constructor(target, options = {}) {
     this.target = target;
-    // Merge default options, provided options, and data attributes
     this.opts = { ...EasyZoom.defaults, ...options, ...this.getDataOptions() };
     this._init();
   }
 
-  // Default options
   static defaults = {
     loadingNotice: "Loading image",
     errorNotice: "The image could not be loaded",
     errorDuration: 2500,
     linkAttribute: "href",
     preventClicks: true,
-    beforeShow: () => {},
-    beforeHide: () => {},
     onShow: () => {},
     onHide: () => {},
     onMove: () => {},
   };
 
-  // Extract options from data attributes
   getDataOptions() {
     const dataset = this.target.dataset;
     const options = {};
@@ -32,7 +27,6 @@ export default class EasyZoom {
     return options;
   }
 
-  // Initialize the EasyZoom functionality
   _init() {
     this.isOpen = false;
     this.isReady = false;
@@ -40,71 +34,73 @@ export default class EasyZoom {
     this.link = this.target.querySelector("a");
     this.image = this.target.querySelector("img");
 
-    // Create the flyout element
     this.flyout = document.createElement("div");
     this.flyout.className = "easyzoom-flyout";
 
     this.target.style.position = "relative";
     this.target.appendChild(this.flyout);
 
-    // Add event listeners
     this.target.addEventListener("mouseenter", this._onEnter.bind(this));
     this.target.addEventListener("mousemove", this._onMove.bind(this));
     this.target.addEventListener("mouseleave", this._onLeave.bind(this));
 
-    // We don't load the zoom image immediately (lazy loading)
+    // Add touch event listeners
+    this.target.addEventListener("touchstart", this._onEnter.bind(this));
+    this.target.addEventListener("touchmove", this._onMove.bind(this));
+    this.target.addEventListener("touchend", this._onLeave.bind(this));
+
+    // Prevent default link behavior
+    this.link.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+
+    this._loadZoomImage();
   }
 
-  // Handler for mouse enter event
-  _onEnter(e) {
-    if (!this.isReady) {
-      // Load the zoom image only when the user enters for the first time
-      this._loadZoomImage(() => {
-        this.isOpen = true;
-        this.target.classList.add("is-active");
-        this._move(e);
-      });
-    } else {
-      this.isOpen = true;
-      this.target.classList.add("is-active");
-      this._move(e);
-    }
-  }
-
-  // Load the zoom image
-  _loadZoomImage(callback) {
-    if (this.isReady) {
-      callback();
-      return;
-    }
-
+  _loadZoomImage() {
     this.zoomImg = new Image();
     this.zoomImg.src = this.link.getAttribute(this.opts.linkAttribute);
     this.zoomImg.onload = () => {
       this.isReady = true;
       this.flyout.appendChild(this.zoomImg);
       this.zoomImg.style.position = "absolute";
-      callback();
     };
   }
 
-  // Handler for mouse move event
+  _onEnter(e) {
+    e.preventDefault();
+    if (!this.isReady) return;
+    this.isOpen = true;
+    this.target.classList.add("is-active");
+    this._move(e);
+  }
+
   _onMove(e) {
+    e.preventDefault();
     if (!this.isOpen || !this.isReady) return;
     this._move(e);
   }
 
-  // Handler for mouse leave event
-  _onLeave() {
+  _onLeave(e) {
     this.isOpen = false;
     this.target.classList.remove("is-active");
   }
 
-  // Calculate and apply the zoom image position
   _move(e) {
+    let pointerX, pointerY;
+
+    if (e.type.startsWith("touch")) {
+      const touch = e.touches[0] || e.changedTouches[0];
+      pointerX = touch.clientX;
+      pointerY = touch.clientY;
+    } else {
+      pointerX = e.clientX;
+      pointerY = e.clientY;
+    }
+
     const targetRect = this.target.getBoundingClientRect();
-    const relativePositionX = (e.clientX - targetRect.left) / targetRect.width;
-    const relativePositionY = (e.clientY - targetRect.top) / targetRect.height;
+    const relativePositionX = (pointerX - targetRect.left) / targetRect.width;
+    const relativePositionY = (pointerY - targetRect.top) / targetRect.height;
 
     const moveX = -(
       (this.zoomImg.width - this.flyout.offsetWidth) *
@@ -119,15 +115,16 @@ export default class EasyZoom {
     this.opts.onMove.call(this, moveX, moveY);
   }
 
-  // Remove event listeners and clean up
   teardown() {
     this.target.removeEventListener("mouseenter", this._onEnter);
     this.target.removeEventListener("mousemove", this._onMove);
     this.target.removeEventListener("mouseleave", this._onLeave);
+    this.target.removeEventListener("touchstart", this._onEnter);
+    this.target.removeEventListener("touchmove", this._onMove);
+    this.target.removeEventListener("touchend", this._onLeave);
     this.flyout.remove();
   }
 
-  // Swap the current image with a new one
   swap(standardSrc, zoomHref) {
     this.isReady = false;
 
@@ -137,7 +134,18 @@ export default class EasyZoom {
     // Update the zoom link
     this.link.setAttribute(this.opts.linkAttribute, zoomHref);
 
-    // We don't load the new zoom image immediately
-    // It will be loaded when the user hovers over it again
+    // Load the new zoom image
+    const newZoomImg = new Image();
+    newZoomImg.src = zoomHref;
+    newZoomImg.onload = () => {
+      this.zoomImg = newZoomImg;
+      this.zoomImg.style.position = "absolute";
+
+      // Clear the flyout and add the new image
+      this.flyout.innerHTML = "";
+      this.flyout.appendChild(this.zoomImg);
+
+      this.isReady = true;
+    };
   }
 }
